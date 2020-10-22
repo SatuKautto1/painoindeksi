9bdcd361e9c980cac14564629ce15774b865faec
SatuKautto1 <72457484+SatuKautto1@users.noreply.github.com>
2020-10-22 13:30:26 +0300

    Update PainoindeksiOhjelma.java

--- PainoindeksiOhjelma.java
+++ PainoindeksiOhjelma.java
@@ -1,1 +1,26 @@
-// Painoindeksiohjelma
+/**
+ * PainoindeksiOhjelma
+ * Asiakas: Paino OY
+ * @authors 
+ * Satu Kautto
+ * Sauli Karvinen
+ * Saara Nyyssönen
+ * Riku Rissanen
+ * Sami Ruotsalainen
+ 
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class PainoindeksiOhjelma {
+    
+    public static void main (String[]args) {
+        Scanner lukija;
+        lukija = new Scanner(System.in);
+      
+        
+       
+        
+    }   
+}
